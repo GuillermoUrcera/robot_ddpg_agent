06b24bfd04f2f13afe97e93e9cce798c66b80905
Guillermo <g.urcera.martin@gmail.com>
2018-04-01 23:18:35 +0200

    Created pendulum_evaluator

--- src/pendulum_evaluator.py
+++ src/pendulum_evaluator.py
@@ -1,37 +1,26 @@
-import gym
-import actor
-import critic
+import pendulum_parameters
 import tensorflow as tf
 import numpy as np
+import actor
 
+# Loaded parameters:
+env=pendulum_parameters.env
+STATE_SIZE=pendulum_parameters.STATE_SIZE
+ACTION_SIZE=pendulum_parameters.ACTION_SIZE
+ACTOR_LEARNING_RATE=pendulum_parameters.ACTOR_LEARNING_RATE
+ACTION_RANGE=pendulum_parameters.ACTION_RANGE
+HIDDEN_SIZE_ACTOR=pendulum_parameters.HIDDEN_SIZE_ACTOR
+ACTOR_NAME=pendulum_parameters.ACTOR_NAME
+ACTOR_SUBSPACE_NAME=pendulum_parameters.ACTOR_SUBSPACE_NAME
+MINIBATCH_SIZE=pendulum_parameters.MINIBATCH_SIZE
+SAVE_PATH=pendulum_parameters.SAVE_PATH
 
-#Actor hyperparameters
-ACTOR_LEARNING_RATE=1e-4
-HIDDEN_SIZE_ACTOR=8
-ACTOR_NAME="actor"
-ACTOR_SUBSPACE_NAME="ACTOR_OPS"
-ACTOR_TARGET_NAME="actor_target"
-ACTOR_TARGET_SUBSPACE_NAME="TARGET_ACTOR_OPS"
-#Environment
-ENVIRONMENT='Pendulum-v0'
-env=gym.make(ENVIRONMENT)
-ACTION_RANGE=env.action_space.high[0]
-STATE_SIZE=env.observation_space.shape[0]
-ACTION_SIZE=env.action_space.shape[0]
-#Memory
-MINIBATCH_SIZE=1024
-MEMORY_MAX_SIZE=int(1e5)
-INDEX_STATE=0
-INDEX_REWARD=1
-INDEX_DONE=2
-INDEX_LAST_STATE=3
-INDEX_ACTION=4
-VAR_SIZE_DIC={INDEX_STATE:STATE_SIZE,
-              INDEX_REWARD:1,
-              INDEX_DONE:1,
-              INDEX_LAST_STATE:STATE_SIZE,
-              INDEX_ACTION:ACTION_SIZE}
+# Evaluator parameters:
+EPISODES_TO_EVALUATE=5
+EPOCHS_PER_EPISODE_TO_EVALUATE=500
+VISUALIZE_EVALUATOR=True
 
+# Program start
 tf.reset_default_graph()
 sess=tf.Session()
 
@@ -41,12 +30,12 @@ with tf.variable_scope(ACTOR_SUBSPACE_NAME):
 
 saver = tf.train.Saver()
 
-
 # Restore variables from disk.
-saver.restore(sess, "/tmp/model.ckpt")
-for e in range(5):
+saver.restore(sess,SAVE_PATH)
+for e in range(EPISODES_TO_EVALUATE):
 	state=env.reset()
-	for i in range(500):
-		env.render()
+	for i in range(EPOCHS_PER_EPISODE_TO_EVALUATE):
+		if VISUALIZE_EVALUATOR:
+			env.render()
 		state, reward, done, info=env.step(my_actor.predict(np.reshape(state,(1,STATE_SIZE))))
 	env.render(close=True)
