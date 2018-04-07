f75ac7851f665b4fb111800bf884a6fd1b144a20
Guillermo <g.urcera.martin@gmail.com>
2018-04-07 17:37:30 +0200

    Gazebo_accuracy_evaluator now outputs time elapsed

--- src/gazebo_accuracy_evaluator.py
+++ src/gazebo_accuracy_evaluator.py
@@ -3,23 +3,29 @@
 import gazebo_parameters
 import numpy as np
 import csv
+import time
 
 # Loaded parameters:
 env=gazebo_parameters.env
 
 
 # Evaluator parameters:
-EPISODES_TO_EVALUATE=10
+EPISODES_TO_EVALUATE=1000
 
 state_to_evaluate=[2,2,5,2,10,2]
 action_to_evaluate=[[2.0,-2.0,4.0]]
 
-f = open("out.csv","wb")
+f = open("out_0_0005.csv","wb")
 cr = csv.writer(f,delimiter=';')
-for e in range(EPISODES_TO_EVALUATE):
+start = time.time()
+for episode in range(EPISODES_TO_EVALUATE):
 	env.state=state_to_evaluate
 	reward=env.step(action_to_evaluate)
 	cr.writerow([reward])
+	if episode%100==0:
+		print "episode ",episode
 f.close()	
+end = time.time()
+print end-start
 
 
