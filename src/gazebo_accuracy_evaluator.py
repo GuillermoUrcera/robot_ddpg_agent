82ca7dbec746a0847f78bdcdbc179c5401d070c1
Guillermo <g.urcera.martin@gmail.com>
2018-04-17 22:46:50 +0200

    removed a print

--- src/gazebo_accuracy_evaluator.py
+++ src/gazebo_accuracy_evaluator.py
@@ -14,17 +14,16 @@ EPISODES_TO_EVALUATE=1000
 state_to_evaluate=[2,2,5,2,10,2]
 action_to_evaluate=[[2.0,-2.0,4.0]]
 
-f = open("out_0_0005.csv","wb")
+f = open("out_0_0001.csv","wb")
 cr = csv.writer(f,delimiter=';')
 start = time.time()
 for episode in range(EPISODES_TO_EVALUATE):
 	env.obstacle_positions=state_to_evaluate
 	reward=env.step(action_to_evaluate)
-	print reward
 	cr.writerow([reward])
 	if episode%100==0:
 		print "episode ",episode
-f.close()	
+f.close()
 end = time.time()
 print end-start
 
