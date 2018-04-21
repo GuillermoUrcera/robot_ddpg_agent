9e8cbee27cd4d9202fb3f689583cb88a74a784b9
Guillermo <g.urcera.martin@gmail.com>
2018-04-21 16:30:58 +0200

    Deleted commented line

--- src/critic.py
+++ src/critic.py
@@ -27,7 +27,6 @@ class Critic:
     def createCritic(self,state_size,action_size):
         # TODO  change layers in which action and state are introduced
         with tf.variable_scope(self.name+"_network"):
-            #critic_input=tf.layers.flatten(tf.concat([self.state_input_tensor,self.action_input_tensor],1),name="concat_layer")
             critic_input=tf.concat([self.state_input_tensor,self.action_input_tensor],1)
             h1=tf.layers.dense(critic_input,self.hidden_size,activation=tf.nn.relu,name="hidden_layer_1",reuse=False)
             h2=tf.layers.dense(h1,self.hidden_size,activation=tf.nn.relu,name="hidden_layer_2",reuse=False)
